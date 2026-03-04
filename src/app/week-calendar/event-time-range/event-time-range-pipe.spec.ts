11e59002bcc5922bb5de9c22a9f773e06e730a31
barzeli <evya789@gmail.com>
2026-03-04 16:54:15 +0200

    removed tests

--- src/app/week-calendar/event-time-range/event-time-range-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EventTimeRangePipe } from './event-time-range-pipe';
-
-describe('EventTimeRangePipe', () => {
-  it('create an instance', () => {
-    const pipe = new EventTimeRangePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
